83e441d5bc336ffa8caecc1380d1d3031713f88f
vladimir <volobud@gmail.com>
2015-01-18 01:20:31 +0200

    Some work with Spinner.

--- app/src/main/java/com/vll/volobud/converter/Converter.java
+++ app/src/main/java/com/vll/volobud/converter/Converter.java
@@ -4,16 +4,43 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
-public class Converter extends ActionBarActivity {
+public class Converter extends ActionBarActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_converter);
         //start
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        //finish
     }
+
+        public void onItemSelected(AdapterView<?> parent, View view,
+        int pos, long id) {
+            // An item was selected. You can retrieve the selected item using
+            // parent.getItemAtPosition(pos)
+            Toast.makeText(getBaseContext(), "Position = " + pos, Toast.LENGTH_SHORT).show();
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
+
 
 
     @Override
