f3641df9f9edc02c836ff59dbe36b3be3ea51688
volobud <volobud@gmail.com>
2015-02-04 17:48:57 +0200

    Added listener on button to basic fragment.

--- app/src/main/java/com/vll/volobud/converter/BasicFragment.java
+++ app/src/main/java/com/vll/volobud/converter/BasicFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by budnik on 20.01.15.
@@ -15,7 +17,17 @@ public class BasicFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.basic_fragment, container, false);
+        //return inflater.inflate(R.layout.basic_fragment, container, false);
         //return inflater.inflate(R.layout.basic_fragment, null);
+        View v = inflater.inflate(R.layout.basic_fragment, null);
+
+        Button button = (Button) v.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Fragment", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return v;
     }
 }
