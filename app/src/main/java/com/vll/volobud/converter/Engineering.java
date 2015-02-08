f60a2788af75fcbfbaf64036475a88456573f66a
Bender <Chernenko_vitaliy@ukr.net>
2015-02-08 17:30:22 +0200

    Added fragment engineering and listener EditText

--- app/src/main/java/com/vll/volobud/converter/Engineering.java
+++ app/src/main/java/com/vll/volobud/converter/Engineering.java
@@ -1,20 +1,31 @@
 package com.vll.volobud.converter;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * Created by Bender on 31.01.2015.
- */
 public class Engineering extends Fragment {
 
     @Nullable
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.engineering, container, false);
+        View v = inflater.inflate(R.layout.engineering, container, false);
+
+        Button buttonTime = (Button)v.findViewById(R.id.buttonTime);
+
+        buttonTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Engineering.this.getActivity(), Time_calculation.class);
+                Engineering.this.startActivity(intent);
+            }
+        });
+
+        return  v;
     }
 
 }
--- app/src/main/java/com/vll/volobud/converter/time_calculation.java
+++ app/src/main/java/com/vll/volobud/converter/time_calculation.java
@@ -2,16 +2,115 @@ package com.vll.volobud.converter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * Created by Bender on 01.02.2015.
- */
-public class time_calculation extends Activity {
+public class Time_calculation extends Activity {
+
+    Button buttonClean, buttonNanosecond, buttonMicrosecond, buttonMillisecond;
+    EditText editTextNanosecond, editTextMicrosecond, editTextMillisecond;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.time_calculation);
+
+        buttonClean = (Button)findViewById(R.id.buttonClean);
+        buttonNanosecond = (Button)findViewById(R.id.buttonNanosecond);
+        buttonMicrosecond = (Button)findViewById(R.id.buttonMicrosecond);
+        buttonMillisecond = (Button)findViewById(R.id.buttonMillisecond);
+
+        editTextNanosecond = (EditText)findViewById(R.id.editTextNanosecond);
+        editTextMicrosecond = (EditText)findViewById(R.id.editTextMicrosecond);
+        editTextMillisecond = (EditText)findViewById(R.id.editTextMillisecond);
+
+
+        buttonClean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextNanosecond.setText(null);
+                editTextMicrosecond.setText(null);
+                editTextMillisecond.setText(null);
+            }
+        });
+
+
+
+        editTextNanosecond.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                enableSubmitIfReady();
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+
+        editTextMicrosecond.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                enableSubmitIfReady();
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+
+        editTextMillisecond.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                enableSubmitIfReady();
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+
+
+    }
+
+    public void enableSubmitIfReady() {
+
+        boolean isReadyNano = editTextNanosecond.getText().toString().length() >= 1;
+        boolean isReadyMicro = editTextMicrosecond.getText().toString().length() >= 1;
+        boolean isReadyMili = editTextMillisecond.getText().toString().length() >= 1;
+
+        if (isReadyNano) {
+            buttonNanosecond.setEnabled(true);
+        } else {
+            buttonNanosecond.setEnabled(false);
+        }
+
+
+        if (isReadyMicro) {
+            buttonMicrosecond.setEnabled(true);
+        } else {
+            buttonMicrosecond.setEnabled(false);
+        }
+
+
+        if (isReadyMili) {
+            buttonMillisecond.setEnabled(true);
+        } else {
+            buttonMillisecond.setEnabled(false);
+        }
+
+        if ((isReadyMili) || (isReadyMicro) || (isReadyNano))  {
+            buttonClean.setEnabled(true);
+        } else {
+            buttonClean.setEnabled(false);
+        }
     }
 }
