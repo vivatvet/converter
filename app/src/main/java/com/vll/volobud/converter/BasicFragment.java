090b67de60b2160cf6632ed23f79ded94be74846
volobud <volobud@gmail.com>
2015-02-09 13:37:08 +0200

    Added Weight Activity.

--- app/src/main/java/com/vll/volobud/converter/BasicFragment.java
+++ app/src/main/java/com/vll/volobud/converter/BasicFragment.java
@@ -1,12 +1,12 @@
 package com.vll.volobud.converter;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 /**
  * Created by budnik on 20.01.15.
@@ -16,15 +16,14 @@ public class BasicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.basic_fragment, container, false);
-        //return inflater.inflate(R.layout.basic_fragment, null);
+
         View v = inflater.inflate(R.layout.basic_fragment, null);
 
-        Button button = (Button) v.findViewById(R.id.button);
+        Button button = (Button) v.findViewById(R.id.button_weight);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "Fragment", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(BasicFragment.this.getActivity(), Weight_calculation.class);
+                BasicFragment.this.startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/com/vll/volobud/converter/Weight_calculation.java
@@ -0,0 +1,16 @@
+package com.vll.volobud.converter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by budnik on 09.02.15.
+ */
+public class Weight_calculation extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weight_calculation);
+
+    }
+}
